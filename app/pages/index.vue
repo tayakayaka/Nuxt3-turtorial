9a3ad259c67a2dba87e2131acd99bf5dc60dfc5f
Iroha <b18421ka@gm.tsuda.ac.jp>
2025-08-04 13:25:04 +0900

    assetsの設定

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,5 @@
 <script lang="ts" setup>
-definePageMeta({
-  layout: "auth-layout"
-})
+
 </script>
 
 <template>
@@ -12,4 +10,21 @@ definePageMeta({
   <CommonAppPrimary>
     OKです
   </CommonAppPrimary>
+  <div class="header">
+    My Nuxt App
+  </div>
+  <img src="/images/symba.png" alt="symba">
 </template>
+
+<style lang="scss" scoped>
+img {
+  width: 200px;
+  height: auto; /* アスペクト比を維持 */
+}
+.header{
+  background-color:$primary;
+  color:$secondary;
+  padding: 20px;
+  text-align: center;
+}
+</style>
